92be6933bd4c5196d7d1b0680b28304e5dac6abe
Baejiu <jiumaker@naver.com>
2025-01-14 16:22:07 +0900

    feat: add image, note, todo, video static item

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,37 @@
-import { PageComponent } from './components/page.js';
+import { Component } from './components/component.js';
+import { ImageComponent } from './components/item/image.js';
+import { NoteComponent } from './components/item/note.js';
+import { TodoComponent } from './components/item/todo.js';
+import { VideoComponent } from './components/item/video.js';
+import {
+  Composable,
+  PageComponent,
+  PageItemComponent,
+} from './components/page/page.js';
 
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement) {
-    this.page = new PageComponent();
+    this.page = new PageComponent(PageItemComponent);
     this.page.attachTo(appRoot);
+
+    const image = new ImageComponent(
+      'Image Title',
+      'https://picsum.photos/600/300'
+    );
+    this.page.addChild(image);
+
+    const note = new NoteComponent('NOte Title', 'note body');
+    this.page.addChild(note);
+
+    const todo = new TodoComponent('todo title', 'todo item');
+    this.page.addChild(todo);
+
+    const video = new VideoComponent(
+      'title',
+      'https://www.youtube.com/embed/jim9K1vBL0k'
+    );
+    this.page.addChild(video);
   }
 }
 
